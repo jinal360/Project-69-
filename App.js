25786c2cbc72f18ebc2946e0766b2b9ba0ac496d
jinal360 <73110780+jinal360@users.noreply.github.com>
2021-03-31 18:42:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator}from 'react-navigation-tabs';
+import facebook from './screens/facebook';
+import insta from './screens/insta';
+import ScanScreen from './screens/ScanScreen';
+
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <AppContainer/>
+    )
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  facebook:{screen:facebook},
+  insta:{screen:insta}
+})
+
+const AppContainer = createAppContainer(TabNavigator)
